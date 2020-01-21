28c2e1da6b3eaeba986b3c8f213c592174fba992
cetric <cetric32@gmail.com>
2020-01-21 11:35:11 +0300

    Initial commit

---
+++ src/js/people.js
@@ -0,0 +1,11 @@
+/*function getHello(){
+    return "Hello lovely people";
+}
+*/
+let people = [
+    {name: 'cetric lihalakha'},
+    {name: 'teddy Seth'},
+    {name: 'carol williams'}
+]
+
+module.exports = people;
